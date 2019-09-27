d424e9098abdb71a92738cde6f4d9b2fa30f85f9
Lucas Barbati <200241833@student.georgianc.on.ca>
2019-09-27 08:01:48 -0700

    cmd to unpack tgz, add to right d, and rm file

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -17,6 +17,7 @@ test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net
 
 # unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -o -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
+test -f ~/Pictures/pics.tgz && tar -zxf ~/Pictures/pics.tgz -C ~/Pictures && rm ~/Pictures/pics.tgz
 
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
